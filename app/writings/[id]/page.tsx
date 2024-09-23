39591d3878311b9c169a042ed41f265f0cb3be15
vinay putta <vinayputta@vinays-MacBook-Air.local>
2024-09-23 22:11:01 +0530

    build issue resolve

--- app/writings/[id]/page.tsx
+++ app/writings/[id]/page.tsx
@@ -1,19 +1,29 @@
 import { getPostData, getAllPostIds } from '../../../lib/writings'
+
+interface PostData {
+    title: string;
+    date: string;
+    contentHtml: string;
+}
+
 export async function generateStaticParams() {
     const paths = getAllPostIds()
-    return paths
+    return paths.map((path) => ({
+        id: path.params.id,
+    }))
 }
 
 export default async function Post({ params }: { params: { id: string } }) {
-    const postData = await getPostData(params.id)
+    const postData: PostData = await getPostData(params.id)
 
     return (
         <div className="container mx-auto px-4">
             <h1 className="text-3xl font-bold my-4 font-lato">{postData.title}</h1>
             <p className="text-gray-500 font-montserrat">{postData.date}</p>
-            <div className="mt-4 font-lato markdown-content" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-
+            <div
+                className="mt-4 font-lato markdown-content"
+                dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+            />
         </div>
     )
 }
-
